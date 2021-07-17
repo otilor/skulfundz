137661fb3d714e6bdf5faa757d00007a5673df77
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-07-17 16:28:16 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -204,8 +204,8 @@
                 <h3 class="fs-2hx text-dark mb-5" id="how-it-works" data-kt-scroll-offset="{default: 100, lg: 150}">How it Works</h3>
                 <!--end::Title-->
                 <!--begin::Text-->
-                <div class="fs-5 text-muted fw-bold">Save thousands to millions of bucks by using single tool
-                    <br />for different amazing and great useful admin</div>
+                <div class="fs-5 text-muted fw-bold">Save for the raining day
+                    <br />make sapa no do you strong thing.</div>
                 <!--end::Text-->
             </div>
             <!--end::Heading-->
