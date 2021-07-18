43225d94f4785558366ec4cc61eb15c8b769161f
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-07-18 15:28:07 +0100

    Add logout icon

--- resources/views/new-dashboard.blade.php
+++ resources/views/new-dashboard.blade.php
@@ -190,6 +190,10 @@ License: You must have a valid license purchased only from themeforest(the above
             <!--begin::Footer-->
             <div class="aside-footer d-flex flex-column align-items-center flex-column-auto py-8">
                 <!--begin::Notifications-->
+                <a href="#" class="btn btn-icon btn-lg btn-borderless mb-1 position-relative" id="kt_quick_notifications_toggle" data-toggle="tooltip" data-placement="right" data-container="body" data-boundary="window" title="Logout">
+                    <span class="svg-icon svg-icon-xxl"><!--begin::Svg Icon | path:assets/media/svg/icons/Design/Layers.svg--><!--end::Svg Icon-->
+                        <i class="flaticon-logout"></i>
+                    </span>
                 <a href="#" class="btn btn-icon btn-lg btn-borderless mb-1 position-relative" id="kt_quick_notifications_toggle" data-toggle="tooltip" data-placement="right" data-container="body" data-boundary="window" title="Notifications">
 				<span class="svg-icon svg-icon-xxl"><!--begin::Svg Icon | path:assets/media/svg/icons/Design/Layers.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
